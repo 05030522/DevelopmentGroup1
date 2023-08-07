74ca5c8cbd606597c48f091d673715a742b329cc
05030522 <kjinhx96@gmail.com>
2023-08-07 14:23:49 +0900

    init commit

---
+++ src/main/java/com/sparta/developmentgroup1/DevelopmentGroup1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.developmentgroup1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentGroup1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentGroup1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/developmentgroup1/DevelopmentGroup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.developmentgroup1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentGroup1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
