cdb0a07d554ff747d0a611478f47210361eaf913
canseeit <leekw1245@gmail.com>
2023-08-11 23:50:59 +0900

    회원가입/로그인 , 보드 생성 수정
    http.formLogin 삭제, 요청 접근 허가 경로 수정
    email으로 생성하던 토큰 username사용으로 수정
    회원가입 요청 시 필드에 제한 걸리도록 수정
    보드생성 "exampleUsername" 로그인된 사용자의username 들어가도록 수정

--- src/main/java/com/sparta/developmentgroup1/user/controller/UserController.java
+++ src/main/java/com/sparta/developmentgroup1/user/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.sparta.developmentgroup1.user.controller;
 
 import com.sparta.developmentgroup1.common.dto.ApiResponseDto;
-import com.sparta.developmentgroup1.common.jwt.JwtUtil;
 import com.sparta.developmentgroup1.common.security.UserDetailsImpl;
-import com.sparta.developmentgroup1.user.dto.LoginRequestDto;
 import com.sparta.developmentgroup1.user.dto.SignupRequestDto;
 import com.sparta.developmentgroup1.user.dto.UserInfoDto;
 import com.sparta.developmentgroup1.user.service.UserService;
@@ -12,25 +10,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) throws IllegalAccessException {
+    public ResponseEntity<ApiResponseDto> signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : fieldErrors) {
+                return ResponseEntity.badRequest().body(
+                        new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage())
+                );
+            }
+        }
         userService.signup(requestDto);
         return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "회원 가입 성공"));
-    }
-
-    //로그인
-    @PostMapping("/login")
-    public ResponseEntity<ApiResponseDto> login(@RequestBody LoginRequestDto requestDto) {
-        return null;
     }
 
     @GetMapping("/user-info")
